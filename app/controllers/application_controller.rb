44a0d4b1a989e3ebaedb53de6eb838d314b040a9
hiroki.daitoku <hirokidaitoku@gmail.com>
2020-05-08 11:10:49 +0000

    count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
   
   def counts(user)
     @count_mypets = user.mypets.count if user.mypets.present?
-    @count_followings = user.followings.count
-    @count_followers = user.followers.count
-    @count_posts = user.posts.count
-    @count_favorites = user.favorites.count
+    @count_followings = user.followings.count if user.followings.present?
+    @count_followers = user.followers.count if user.followers.present?
+    @count_posts = user.posts.count if user.posts.present?
+    @count_favorites = user.favorites.count if user.favorites.present?
   end
   
   def require_user_logged_in
